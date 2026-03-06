b8baa4e70f80f374ba7b1b48470329b1e39cef82
saloniurkude414 <saloniurkude414@gmail.com>
2026-03-06 13:51:10 +0530

    add programDefine Constructor. How do we invoke constructor in JAVA?

---
+++ class Test.java
@@ -0,0 +1,12 @@
+class Test
+{
+    Test()
+    {
+        System.out.println("Constructor called");
+    }
+
+    public static void main(String args[])
+    {
+        Test t = new Test();
+    }
+}
